43ee02be04f9b4968ae3244097f2435b5428a955
hassancs44 <hassanalhrbi955@gmail.com>
2026-01-20 10:35:22 +0300

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,24 +1,33 @@
-const CACHE_NAME = "workcar-v1";
+const CACHE_NAME = "workcar-v2";
 
 const URLS_TO_CACHE = [
   "/",
   "/login",
   "/work",
   "/dashboard",
+  "/static/manifest.json",
   "/static/7s.jpg",
-  "/static/manifest.json"
+  "/static/icons/android/android-launchericon-192-192.png",
+  "/static/icons/android/android-launchericon-512-512.png"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(r => r || fetch(event.request))
   );
 });
